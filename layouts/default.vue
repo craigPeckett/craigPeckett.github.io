38562a1ed216fe27e98222e8deb8370f2e62902f
Craig <peckett23@gmail.com>
2020-03-25 17:20:41 +1100

    added landing page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,117 +1,161 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
+  <v-app>
+    <v-app-bar dark app class="primary">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-title class="brand">peckett.io</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn @click="dialog = true" icon>
+        <v-icon>mdi-email</v-icon>
+      </v-btn>
+      <!-- <v-btn @click="changeTheme" icon>
+        <v-icon>{{ icon }}</v-icon>
+      </v-btn> -->
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list-item class="primary">
+        <v-list-item-avatar>
+          <v-img :src="require('@/assets/me.jpg')"></v-img>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title class="navlink">Craig Peckett</v-list-item-title>
+          <v-list-item-subtitle>Full Stack Developer</v-list-item-subtitle>
+        </v-list-item-content>
+        <v-btn @click="drawer = !drawer" icon>
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list dense nav class="navlink">
         <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
+          v-for="nav in navs"
+          :key="nav.title"
+          link
+          :href="nav.link"
+          @click
         >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
+          <v-list-item-icon>
+            <v-icon>{{ nav.icon }}</v-icon>
+          </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
+            <v-list-item-title>{{ nav.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
+      <template v-slot:append>
+        <v-card-actions>
+          <v-btn @click="dialog = true" class="primary" block text
+            >Contact Me</v-btn
+          >
+        </v-card-actions>
+      </template>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
+
+    <Dialog v-model="dialog" />
+
     <v-content>
-      <v-container>
-        <nuxt />
-      </v-container>
+      <nuxt />
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; 2019</span>
-    </v-footer>
+
+    <v-card dark flat tile class="primary text-center">
+      <v-card-text>
+        <v-btn
+          v-for="network in networks"
+          :key="network.icon"
+          :href="network.link"
+          target="_blank"
+          class="mx-4"
+          icon
+        >
+          <v-icon size="24px">{{ network.icon }}</v-icon>
+        </v-btn>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-text>
+        {{ new Date().getFullYear() }} -
+        <span>
+          <a href="mailto:me@craigpeckett.com" class="brand">peckett.io</a>
+        </span>
+      </v-card-text>
+    </v-card>
   </v-app>
 </template>
 
 <script>
+import { mapState } from "vuex";
+import Dialog from "@/components/Dialog";
+
 export default {
-  data () {
+  components: {
+    Dialog
+  },
+  data() {
     return {
-      clipped: false,
+      dialog: false,
       drawer: false,
-      fixed: false,
-      items: [
+      icon: "mdi-lightbulb-on",
+      navs: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          title: "Who am I?",
+          icon: "mdi-account",
+          link: "#who"
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          title: "What can I do?",
+          icon: "mdi-text-box-check",
+          link: "#what"
+        },
+        {
+          title: "How can I do it?",
+          icon: "mdi-microsoft-visual-studio-code",
+          link: "#how"
+        },
+        {
+          title: "Where have I done it?",
+          icon: "mdi-web",
+          link: "#where"
         }
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      networks: [
+        {
+          icon: "mdi-facebook",
+          link: "https://www.facebook.com/craigpeckett.io/"
+        },
+        {
+          icon: "mdi-instagram",
+          link: "https://www.instagram.com/craigpeckett.io/"
+        },
+        {
+          icon: "mdi-linkedin",
+          link: "https://www.linkedin.com/in/craig-peckett-337881128/"
+        },
+        { icon: "mdi-github", link: "https://github.com/craigPeckett" }
+      ]
+    };
+  },
+  computed: {
+    drawerWidth() {
+      return "100%";
+    }
+  },
+  methods: {
+    changeTheme() {
+      if (this.icon === "mdi-lightbulb-on") this.icon = "mdi-lightbulb-off";
+      else this.icon = "mdi-lightbulb-on";
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
     }
   }
-}
+};
 </script>
+<style>
+html {
+  font-size: 16px;
+}
+.brand {
+  font-family: "Lobster", cursive;
+}
+.v-application a {
+  color: white;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,92 +1,183 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
+  <v-container>
+    <v-row id="who" class="text-center" justify="center">
+      <v-col cols="12">
+        <h1>Who am I?</h1>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-avatar size="200">
+          <v-img :src="require('@/assets/me.jpg')"></v-img>
+        </v-avatar>
+        <h2>Craig Peckett</h2>
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
+          Hello... my name is Craig and I am a full stack web developer based in
+          Sydney. I love building web applications from start to finish
+          primarily focusing on simplicity and efficiency.
         </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
-</template>
+        <v-btn @click="dialog = true" class="primary" block>Contact Me</v-btn>
+      </v-col>
+    </v-row>
 
+    <v-row id="what" class="text-center" justify="center">
+      <v-col cols="12">
+        <h1>What can I do?</h1>
+      </v-col>
+      <v-col v-for="item in what" :key="item.title" cols="12" sm="4">
+        <v-icon size="100" color="primary">{{ item.icon }}</v-icon>
+        <h2 class="d-block">{{ item.title }}</h2>
+        <v-card-text>{{ item.text }}</v-card-text>
+      </v-col>
+    </v-row>
+
+    <v-row id="how" justify="center">
+      <v-col cols="12">
+        <h1 class="text-center">How can I do it?</h1>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-list>
+          <v-list-item v-for="item in how" :key="item.title" @click>
+            <v-list-item-icon>
+              <v-icon :color="item.colour">{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title"></v-list-item-title>
+              <v-list-item-subtitle
+                v-text="item.subtitle"
+              ></v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-col>
+    </v-row>
+
+    <v-row id="where">
+      <v-col cols="12" class="text-center">
+        <h1>Where have I done it?</h1>
+        <v-progress-circular
+          :size="150"
+          color="primary"
+          indeterminate
+          class="mt-2"
+        ></v-progress-circular>
+        <v-card-text>Projects coming soon...</v-card-text>
+      </v-col>
+    </v-row>
+    <Dialog v-model="dialog" />
+  </v-container>
+</template>
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import Dialog from "@/components/Dialog";
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
+    Dialog
+  },
+  data() {
+    return {
+      dialog: false,
+      what: [
+        {
+          icon: "mdi-tablet-cellphone",
+          title: "Build",
+          text:
+            "I can build fully responsive web applications that work across all electronic devices and internet browsers."
+        },
+        {
+          icon: "mdi-pencil",
+          title: "Design",
+          text:
+            "I can design fast, simple websites that strengthens your company’s brand whilst presenting the ultimate user experience."
+        },
+        {
+          icon: "mdi-account-check",
+          title: "Customer Satisfaction",
+          text:
+            "I can provide a service of high quality and performance to gain 100% customer satisfaction or money back guaranteed."
+        }
+      ],
+      how: [
+        {
+          icon: "mdi-language-html5",
+          title: "HTML5",
+          subtitle: "Hypertext Markup Language",
+          colour: "red"
+        },
+        {
+          icon: "mdi-language-css3",
+          title: "CSS3",
+          subtitle: "Cascading Style Sheets",
+          colour: "blue"
+        },
+        {
+          icon: "mdi-language-javascript",
+          title: "Javascript",
+          subtitle: "ECMAScript",
+          colour: "yellow"
+        },
+        {
+          icon: "mdi-bootstrap",
+          title: "Bootstrap",
+          subtitle: "Component Library",
+          colour: "purple"
+        },
+        {
+          icon: "mdi-sass",
+          title: "Sass",
+          subtitle: "CSS Language",
+          colour: "pink"
+        },
+        {
+          icon: "mdi-jquery",
+          title: "JQuery",
+          subtitle: "Javascript Library",
+          colour: "blue"
+        },
+        {
+          icon: "mdi-vuejs",
+          title: "Vue JS",
+          subtitle: "Javascript Framework",
+          colour: "#41b883"
+        },
+        {
+          icon: "mdi-vuetify",
+          title: "Vuetify",
+          subtitle: "Vue UI Library",
+          colour: "#1f96f2"
+        },
+        {
+          icon: "mdi-nodejs",
+          title: "Node JS",
+          subtitle: "JavaScript Runtime",
+          colour: "#3c873b"
+        },
+        {
+          icon: "mdi-firebase",
+          title: "Firebase",
+          subtitle: "Application Platform",
+          colour: "#f37f0a"
+        },
+        {
+          icon: "mdi-leaf",
+          title: "Mongo DB",
+          subtitle: "Document Database",
+          colour: "#3c873b"
+        },
+        {
+          icon: "mdi-nuxt",
+          title: "Nuxt JS",
+          subtitle: "Vue JS Framework",
+          colour: "#41b883"
+        }
+      ]
+    };
   }
-}
+};
 </script>
+
+<style>
+h1,
+h2,
+.navlink {
+  font-family: "Bebas Neue", cursive;
+}
+</style>
